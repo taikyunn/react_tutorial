9c1e46a8654e3991a073becbb707c00429d7b36d
taikyunn <taichi_141@me.com>
2022-04-20 09:12:19 +0900

    勝者の判定
    勝者を判定し、勝敗が確定した場合ゲームが終了するようにする。

--- src/index.js
+++ src/index.js
@@ -24,6 +24,11 @@ class Board extends React.Component {
   handleClick(i) {
     // コピーそ作成することで、履歴が残るためデータの変化や、複雑な機能の実装に便利
     const squares = this.state.squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -43,7 +48,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X': 'O');
+    }
+
     return (
       <div>
         <div className="status">{status}</div>
@@ -81,6 +93,27 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+// 照射の判別をする関数
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
